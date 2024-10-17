16582085363c8e893e5ca6b695d9a2896ec4bf37
Saveliy Yudin <yudinsv@agata-hub.ru>
2024-10-17 22:56:53 +0300

    feat: added func created card and delete card;

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,26 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+const deleteCard = (event) => {
+    event.target.parentElement.remove();
+}
+
+const createCard = (cardData, deleteCallback) => {
+    let templateCard = document.querySelector("#card-template").content.cloneNode(true);
+    let card = templateCard.querySelector(".card");
+    let cardImage = card.querySelector(".card__image");
+    let cardTitle = card.querySelector(".card__title");
+    let cardBtnDelete = card.querySelector(".card__delete-button");
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+    cardTitle.textContent = cardData.name;
+    cardBtnDelete.addEventListener("click", deleteCallback);
+    return card
+}
+
+const cards = document.querySelector(".places__list")
+initialCards.forEach(elm => {
+    cards.append(createCard(elm, deleteCard));
+})
